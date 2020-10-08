b5fcb274b23b019461f08f870740f7532719214a
Arnold de Bolster <olybol@live.nl>
2020-10-08 16:24:51 +0200

    test

---
+++ devicetypes/led-smart-grote-lamp/led-smart-grote-lamp.src/led-smart-grote-lamp.groovy
@@ -0,0 +1,41 @@
+/**
+ *  LED SMART Grote lamp
+ *
+ *  Copyright 2020 Arnold de Bolster
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ */
+metadata {
+	definition (name: "LED SMART Grote lamp", namespace: "LED SMART Grote lamp", author: "Arnold de Bolster", cstHandler: true) {
+		capability "Switch Level"
+	}
+
+
+	simulator {
+		// TODO: define status and reply messages here
+	}
+
+	tiles {
+		// TODO: define your main and details tiles here
+	}
+}
+
+// parse events into attributes
+def parse(String description) {
+	log.debug "Parsing '${description}'"
+	// TODO: handle 'level' attribute
+
+}
+
+// handle commands
+def setLevel() {
+	log.debug "Executing 'setLevel'"
+	// TODO: handle 'setLevel' command
+}
